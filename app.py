8e0dc82ad9404367dff05a7ef8b58aa64db23ca3
FoxyAn <118219124+Fox13th@users.noreply.github.com>
2024-07-06 20:50:55 +0300

    Небольшой фикс

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from gevent import monkey
+
+monkey.patch_all()
+
+from gevent.pywsgi import WSGIServer
 from flask import Flask
 
 from core import config
 from api.v1.statistics import router
+
 
 settings = config.get_settings()
 
@@ -12,3 +18,7 @@ app.debug = settings.debug
 app.secret_key = settings.secret_key
 
 app.register_blueprint(router)
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('127.0.0.1', 5000), app)
+    http_server.serve_forever()
--- uwsgi_app.py
+++
@@ -1,9 +0,0 @@
-from gevent import monkey
-monkey.patch_all()
-
-from gevent.pywsgi import WSGIServer
-from app import app
-
-if __name__ == '__main__':
-    http_server = WSGIServer(('127.0.0.1', 5000), app)
-    http_server.serve_forever()
