bebd7de5dd3cf3eadb8cfecf39bd54175174a3f3
FoxyAn <118219124+Fox13th@users.noreply.github.com>
2024-07-11 22:08:52 +0300

    Исправления согласно рекомендациям п 1-3

--- src/api/v1/statistics.py
+++ src/api/v1/statistics.py
@@ -1,4 +1,8 @@
+from http import HTTPStatus
+
+import sentry_sdk
 from flask import request, jsonify, Blueprint
+from sentry_sdk import capture_exception
 
 from core import config
 from schemas.events import Event
@@ -7,6 +11,12 @@ from service.kafka_s import send_to_kafka
 settings = config.get_settings()
 
 router = Blueprint('statistics', __name__, url_prefix='/api/v1/statistics')
+
+sentry_sdk.init(
+    dsn="https://0687df3d7eef5995ed6b59377692711f@o4507584677478400.ingest.de.sentry.io/4507584685342800",
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
 
 @router.route('/send', methods=['POST'])
@@ -18,8 +28,11 @@ def get_data():
             event_data=input_data.event_data
         )
 
-        return jsonify({'status': 'OK'}), 200
+        status = HTTPStatus.OK
 
     except Exception as error:
-        print(error)
-        return jsonify({'status': 'Bad Request'}), 400
+        capture_exception(error)
+
+        status = HTTPStatus.BAD_REQUEST
+
+    return jsonify({'status': status.phrase}), status
