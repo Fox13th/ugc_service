6058de90e5333d61a1b940e5e551c61e1168acdd
FoxyAn <118219124+Fox13th@users.noreply.github.com>
2024-07-28 16:06:38 +0300

    Update

--- tests_db/psql.py
+++ tests_db/psql.py
@@ -12,17 +12,16 @@ def create_table():
     cur.execute("""
                     CREATE TABLE IF NOT EXISTS test_table (
                         id SERIAL PRIMARY KEY,
-                        data TEXT
+                        user_id TEXT,
+                        movie_id TEXT,
+                        score INT
                     )
                     """)
     pg_conn.commit()
 
 
 def write_data(num: int):
-    data_to_insert = [(f'data_{i}',) for i in range(1, 1000000)]
-    batch_size = 1000
-    total_rows = len(data_to_insert)
-    num_batches = total_rows // batch_size
+    data_to_insert = [(str(uuid.uuid4()), str(uuid.uuid4()), i) for i in range(num)]
 
     start_time = time.time()
 
@@ -39,8 +38,8 @@ def write_data(num: int):
             list_data = []
             #for i in range(chunk):
                 #data_id = get_random(random_user_id, random_movie_id)
-                #data = {'movie_id': f'{data_id[1]}', 'user_id': f'{data_id[0]}', 'score': data_id[2]}
-            for i in range(1, 1000):
+                #data = {'movie_id': f'{data_id[1]}', 'user_id': f'{data_id[0]}', 'score': [2]}
+            for i in range(1000):
                 list_data.append(data_to_insert[i])
 
             execute_values(cur, "INSERT INTO test_table (data) VALUES %s", list_data)
@@ -51,8 +50,7 @@ def write_data(num: int):
             break
     end_time = time.time()
 
-    print(f"Total time for inserting {total_rows} rows in batches of {batch_size}: {end_time - start_time} seconds")
-    print(f"Average time per {batch_size} rows: {(end_time - start_time) / num_batches} seconds")
+    print(f"Total time for inserting {num} rows in batches of {chunk}: {end_time - start_time} seconds")
 
 
 def read_data():
