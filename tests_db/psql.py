81b318e3378622e394dc90f03ff80edece5ab213
FoxyAn <118219124+Fox13th@users.noreply.github.com>
2024-07-28 20:07:34 +0300

    тесты psql.py

--- tests_db/psql.py
+++ tests_db/psql.py
@@ -1,4 +1,5 @@
-import os
+import logging
+import random
 import sys
 import uuid
 from contextlib import closing
@@ -35,8 +36,8 @@ def write_data(num: int):
     if num < chunk:
         chunk = num
 
-    random_user_id = [str(uuid.uuid4()) for i in range(int(sys.argv[2]))]  # 100000
-    random_movie_id = [str(uuid.uuid4()) for i in range(int(sys.argv[3]))]  # 60000
+    random_user_id = [str(uuid.uuid4()) for i in range(int(sys.argv[5]))]  # 100000
+    random_movie_id = [str(uuid.uuid4()) for i in range(int(sys.argv[6]))]  # 60000
 
     while i_num < num:
         try:
@@ -46,30 +47,37 @@ def write_data(num: int):
                 data_to_insert = [data_id[0], data_id[1], data_id[2]]
                 list_data.append(data_to_insert)
 
-            execute_values(cur, "INSERT INTO test_table (data) VALUES %s", list_data)
+            execute_values(cur, "INSERT INTO test_table (user_id, movie_id, score) VALUES %s", list_data)
             pg_conn.commit()
             i_num += 1000
-            print(i_num)
         except IndexError:
             break
     end_time = time.time()
 
-    print(f"Total time for inserting {num} rows in batches of {chunk}: {end_time - start_time} seconds")
+    logging.info(f"Total time for inserting {num} rows in batches of {chunk}: {end_time - start_time} seconds")
+    return random_user_id[0], random_movie_id[1]
 
 
-def read_data():
+def read_data(query: str):
     start_time = time.time()
-    cur.execute("SELECT * FROM test_table")
+    cur.execute(query)
     rows = cur.fetchall()
     end_time = time.time()
-    print(f"Total time for reading {len(rows)} rows: {end_time - start_time} seconds")
+    logging.info(f"Total time for reading {len(rows)} rows: {end_time - start_time} seconds")
+
+
+def drop_table():
+    cur.execute("DROP TABLE test_table")
+    pg_conn.commit()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(levelname)s] %(message)s")
+
     dsl = {
-        'dbname': 'movies_database',
-        'user': 'postgres',
-        'password': '123qwe',
+        'dbname': sys.argv[1],
+        'user': sys.argv[2],
+        'password': sys.argv[3],
         'host': '127.0.0.1',
         'port': 5432,
     }
@@ -77,5 +85,11 @@ if __name__ == '__main__':
     with closing(psycopg2.connect(**dsl, cursor_factory=DictCursor)) as pg_conn:
         cur = pg_conn.cursor()
         create_table()
-        write_data(sys.argv[1])
-        read_data()
+        u_id, m_id = write_data(int(sys.argv[4]))
+        read_data("SELECT * FROM test_table")
+        read_data(
+            f"SELECT movie_id, AVG(score) FROM test_table Where movie_id='{m_id}' GROUP BY movie_id;")
+        read_data(
+            f"SELECT Count(*) FROM test_table Where user_id='{u_id}' and score=1 GROUP BY user_id;")
+        read_data(f"SELECT movie_id FROM test_table Where user_id='{u_id}' and score=1;")
+        drop_table()
